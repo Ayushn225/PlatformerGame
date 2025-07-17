863a567290689f67243dee1897bfe8242d2877ab
Ayushn225 <ayushn454@gmail.com>
2025-07-17 16:08:25 +0530

    input and rigidbody
    space input
    rigid body
    movement and flip

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField] private float speed;
+    private float speedMultiplier = 0;
+
+    [Range(0, 10)]
+    [SerializeField] private float acceleration;
+
+    private bool buttonPressed = false;
+
+    private bool isWallTouch = false;
+    public LayerMask wallLayer;
+    public Transform WallCheckPoint;
+
+    Vector2 relativeTransform;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        updateInverseVector();
+    }
+
+    void FixedUpdate()
+    {
+        updateSpeedMultiplier();
+        float targetSpeed = speed * speedMultiplier*relativeTransform.x;
+        rb.linearVelocity = new Vector2(targetSpeed, rb.linearVelocity.y);
+
+        isWallTouch = Physics2D.OverlapBox(WallCheckPoint.position, 
+            new Vector2(0.1f, 1f),
+            0, 
+            wallLayer
+        );
+
+        if(isWallTouch)
+        {
+            flip();
+        }
+    }
+
+    private void flip(){
+        transform.Rotate(0, 180, 0);
+        updateInverseVector();
+    }
+
+    private void updateInverseVector(){
+        relativeTransform = transform.InverseTransformVector(Vector2.one);
+    }
+    public void move(InputAction.CallbackContext value)
+    {
+        if (value.started)
+        {
+            buttonPressed = true;
+        }
+        else if (value.canceled)
+        {
+            buttonPressed = false;
+        }
+    }
+
+    private void updateSpeedMultiplier(){
+        if(buttonPressed && speedMultiplier < 1){
+            speedMultiplier += Time.deltaTime* acceleration;
+        }else if(!buttonPressed && speedMultiplier > 0){
+            speedMultiplier -= Time.deltaTime* acceleration;
+            if(speedMultiplier < 0){
+                speedMultiplier = 0;
+            }
+        }
+    }
+}
